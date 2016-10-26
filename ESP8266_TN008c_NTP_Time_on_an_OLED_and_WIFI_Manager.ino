721877e0337aac66f007464c820f3866842f878d
G6EJD <d.l.bird@sky.com>
2016-10-26 22:09:07 +0100

    Update ESP8266_TN008c_NTP_Time_on_an_OLED_and_WIFI_Manager.ino

--- ESP8266_TN008c_NTP_Time_on_an_OLED_and_WIFI_Manager.ino
+++ ESP8266_TN008c_NTP_Time_on_an_OLED_and_WIFI_Manager.ino
@@ -2,7 +2,7 @@
 // ESP8266 Clock displayed on an OLED shield (64x48) using the Network Time Protocol to update every minute
 // (c) D L Bird 2016
 //
-#include <NTPClient.h> // https://github.com/gmag11/NtpClient
+#include <NTPClient.h> https://github.com/arduino-libraries/NTPClient
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 #include <WiFiUdp.h>
