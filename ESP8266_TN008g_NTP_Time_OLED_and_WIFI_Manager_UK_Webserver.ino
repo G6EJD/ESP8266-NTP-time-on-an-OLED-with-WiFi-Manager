01df32b899299588b21358db3bd7f5d385645b72
G6EJD <d.l.bird@sky.com>
2017-03-30 10:27:07 +0100

    Add files via upload

--- ESP8266_TN008g_NTP_Time_OLED_and_WIFI_Manager_UK_Webserver.ino
+++ ESP8266_TN008g_NTP_Time_OLED_and_WIFI_Manager_UK_Webserver.ino
@@ -31,7 +31,7 @@ float TimeZone;
 bool  AMPM = false;
 int   epoch,local_epoch,current_year,current_month,current_day,dayOfWeek,hours,UTC_hours,minutes,seconds;
 byte  set_status;
-bool  DST = false, dstUK, dstUSA, dstAUS,output_pin_state; // DayLightSaving on/off and selected zone indicators
+bool  DST = false, dstUK, dstUSA, dstAUS, output_pin_state; // DayLightSaving on/off and selected zone indicators
 #define output_pin D1
 
 // If your country is ahead of UTC by 2-hours use 2, or behind UTC by 2-hours use -2
@@ -56,15 +56,22 @@ void setup(){
   //  7 - dst_USA true/false
   //  8 - dst_AUS true/false
   EEPROM.get(0,TimeZone);
-  EEPROM.get(4,AMPM);
-  // A simple test of first CPU usage, if first time run the probability of these locations equating to 0 is low, so initilaise the timezone
+  delay(500);
+  AMPM = EEPROM.read(4);
+  delay(500);
+  // A simple test of first CPU usage, if first time run the probability of these locations equating to 0 is low, so initialise the timezone
   if (set_status != 0 ) TimeZone = 0;
-  EEPROM.get(5,set_status); 
-  EEPROM.get(6,dstUK);
-  EEPROM.get(7,dstUSA);
-  EEPROM.get(8,dstAUS);
-  EEPROM.get(9,output_pin_state);
-
+  set_status = EEPROM.read(5); 
+  delay(500);
+  dstUK  = EEPROM.read(6);
+  delay(500);
+  dstUSA = EEPROM.read(7);
+  delay(500);
+  dstAUS = EEPROM.read(8);
+  output_pin_state = EEPROM.read(9);
+  delay(500);
+  EEPROM.commit();
+  EEPROM.end();
   //------------------------------
   //WiFiManager intialisation. Once completed there is no need to repeat the process on the current board
   WiFiManager wifiManager;
@@ -101,7 +108,8 @@ void setup(){
   display.display(); // Update screen
   screen_update.attach_ms(1000, display_time); // Call display update routine every 1-sec. Tick units are 1.024mS adjust as required, I've used 1000mS, but should be 976 x 1.024 = 1-sec
   timeClient.update();                         // NTP updates are allowed no-more than every 60-seconds and the 'display_time' function independently updates the seconds
-  epoch   = timeClient.getEpochTime();         // You can test moving time forward or back by adding or subtracting the required number of seconds 1-day = 24*60*60
+  epoch = timeClient.getEpochTime();           // You can test moving time forward or back by adding or subtracting the required number of seconds 1-day = 24*60*60
+  epoch =  epoch + 3600 * TimeZone;
   server.begin();  // Start the webserver
   Serial.println("Webserver started...");
 
@@ -121,6 +129,7 @@ void loop() {
   if (millis()%300000==0) {
     timeClient.update();                 // NTP updates are allowed no-more than every 60-seconds and the 'display_time' function independently updates the seconds
     epoch   = timeClient.getEpochTime(); // You can test moving time forward or back by adding or subtracting the required number of seconds 1-day = 24*60*60
+    epoch =  epoch + 3600 * TimeZone;
     seconds = epoch % 60;
   }
   server.handleClient();   // Wait for a client to connect and when they do process their requests
@@ -170,25 +179,23 @@ void display_time(){ // Note Ticker called routines cannot get a time update usi
   epoch          = epoch + 1;
   UTC_hours      = epoch % 86400 / 3600;
   if (DST) epoch = epoch + 3600; 
-  epoch = epoch + int(TimeZone * 3600);
   calcDate();
-  epoch = epoch - int(TimeZone * 3600);
   if (DST) epoch = epoch - 3600; 
   DST = false; // Until calculated otherwise as follows:
   if (dstUK) { // For the UK DST begins at 0100 on the last Sunday in March and ends at 0200 on the last Sunday in October
     if ((current_month > 2 && current_month < 9 ) || 
-        (current_month == 2 && ((current_day-dayOfWeek) >= 25) &&  dayOfWeek == 0 && hours >= 1) ||
-        (current_month == 2 && ((current_day-dayOfWeek) >= 25) &&  dayOfWeek >  0) || 
-        (current_month == 9 && ((current_day-dayOfWeek) <= 24) &&  dayOfWeek >  0) ||
-        (current_month == 9 && ((current_day-dayOfWeek) <= 24) && (dayOfWeek == 0 && hours <=2))) DST = true;   
+        (current_month == 2  && ((current_day-dayOfWeek) >= 25) &&  dayOfWeek == 0 && hours >= 1) ||
+        (current_month == 2  && ((current_day-dayOfWeek) >= 25) &&  dayOfWeek >  0) || 
+        (current_month == 9  && ((current_day-dayOfWeek) <= 24) &&  dayOfWeek >  0) ||
+        (current_month == 9  && ((current_day-dayOfWeek) <= 24) && (dayOfWeek == 0 && hours <= 1))) DST = true;   
   }
   if (dstUSA) { // For the USA DST begins at 0200 on the second Sunday of March and ends on the first Sunday of November at 0200 
-    if ((current_month  > 2 &&   current_month < 10 ) || 
-        (current_month == 2 &&  (current_day-dayOfWeek) >= 12 &&  dayOfWeek == 0 && hours > 2) ||
-        (current_month == 2 &&  (current_day-dayOfWeek) >= 12 &&  dayOfWeek >  0) || 
-        (current_month == 10 && (current_day-dayOfWeek) <= 5  &&  dayOfWeek >  0) ||
-        (current_month == 10 && (current_day-dayOfWeek) <= 5  && (dayOfWeek == 0 && hours < 2))) DST = true;   
-  }
+    if ((current_month > 2 && current_month < 10 ) || 
+        (current_month == 2  && ((current_day-dayOfWeek) >= 12) &&  dayOfWeek == 0 && hours >= 2) ||
+        (current_month == 2  && ((current_day-dayOfWeek) >= 12) &&  dayOfWeek > 0) ||
+        (current_month == 10 && ((current_day-dayOfWeek) <= 4)  &&  dayOfWeek > 0) ||
+        (current_month == 10 && ((current_day-dayOfWeek) <= 4)  && (dayOfWeek == 0 && hours <= 1))) DST = true;  
+  } 
   if (dstAUS) { // For Australia DST begins on the first Sunday of October @ 0200 and ends the first Sunday in April @ 0200
     if ((current_month  > 9 || current_month < 3 ) || 
         (current_month == 3 && (current_day-dayOfWeek) <= 0  &&  dayOfWeek == 0 && hours < 3) ||
@@ -217,9 +224,9 @@ void display_time(){ // Note Ticker called routines cannot get a time update usi
   if (AMPM){ if (hours%24 < 12) display.print("AM"); else display.print("PM");}
   display.setCursor(10,41); // Move dayOfWeekn to a position that can accomodate seconds display
   if (DST)    display.print("DST-"); else display.print("UTC-"); 
-  if (dstUK)  display.print("UK");
-  if (dstUSA) display.print("USA");
-  if (dstAUS) display.print("AUS"); 
+  if (dstUK == true)  display.print("UK");
+  if (dstUSA == true) display.print("USA");
+  if (dstAUS == true) display.print("AUS"); 
   display.display(); //Update the screen
 }
 
@@ -277,32 +284,41 @@ void update_webpage(){
 }
 
 void timezone_plus() {
+  EEPROM.begin(9);
   TimeZone += 0.5;
   EEPROM.put(0,TimeZone);
+  delay(500);
   EEPROM.put(5,0); // set_status to a known value
-  EEPROM.commit();
+  delay(500);
+  EEPROM.end();
   update_webpage();
   server.send(200, "text/html", webpage);
 }
 
 void timezone_minus() {
+  EEPROM.begin(9);
   TimeZone -= 0.5;
   EEPROM.put(0,TimeZone);
+  delay(500);
   EEPROM.put(5,0); // set_status to a known value
-  EEPROM.commit();
+  delay(500);
+  EEPROM.end();
   update_webpage();
   server.send(200, "text/html", webpage);
 }
 
 void AMPM_ONOFF() {
+  EEPROM.begin(9);
   if (AMPM) AMPM = false; else AMPM = true;
   EEPROM.put(4,AMPM);
-  EEPROM.commit();
+  delay(500);
+  EEPROM.end();
   update_webpage();
   server.send(200, "text/html", webpage);
 }
 
 void toggle_output() {
+  EEPROM.begin(9);
   if (output_pin_state) output_pin_state = false; else output_pin_state = true;
   if (output_pin_state) { // if true then make it HIGH
     pinMode(output_pin, OUTPUT);
@@ -313,48 +329,62 @@ void toggle_output() {
     digitalWrite(output_pin,LOW);
   }
   EEPROM.put(9,output_pin_state);
-  EEPROM.commit();
+  delay(500);
+  EEPROM.end();
   update_webpage();
   server.send(200, "text/html", webpage);
 }
 
 void dst_UK() {
+  EEPROM.begin(9);
   dstUK  = true;
   dstUSA = false;
   dstAUS = false;
   EEPROM.put(6,dstUK);
+  delay(500);
   EEPROM.put(7,dstUSA);
+  delay(500);
   EEPROM.put(8,dstAUS);
-  EEPROM.commit();
+  delay(500);
+  EEPROM.end();
   update_webpage();
   server.send(200, "text/html", webpage);
 }
 
 void dst_USA() {
+  EEPROM.begin(9);
   dstUK  = false;
   dstUSA = true;
   dstAUS = false;
   EEPROM.put(6,dstUK);
+  delay(500);
   EEPROM.put(7,dstUSA);
+  delay(500);
   EEPROM.put(8,dstAUS);
-  EEPROM.commit();
+  delay(500);
+  EEPROM.end();
   update_webpage();
   server.send(200, "text/html", webpage);
 }
 
 void dst_AUS() {
+  EEPROM.begin(9);
   dstUK  = false;
   dstUSA = false;
   dstAUS = true;
   EEPROM.put(6,dstUK);
+  delay(500);
   EEPROM.put(7,dstUSA);
+  delay(500);
   EEPROM.put(8,dstAUS);
-  EEPROM.commit();
+  delay(500);
+  EEPROM.end();
   update_webpage();
   server.send(200, "text/html", webpage);
 }
 
 void reset_values() {
+  EEPROM.begin(9);
   AMPM             = false;
   TimeZone         = 0;
   set_status       = 0;
@@ -363,13 +393,20 @@ void reset_values() {
   dstAUS           = false;
   output_pin_state = false;
   EEPROM.put(0,TimeZone);
+  delay(500);
   EEPROM.put(4,AMPM);
+  delay(500);
   EEPROM.put(5,set_status);
+  delay(500);
   EEPROM.put(6,dstUK);
+  delay(500);
   EEPROM.put(7,dstUSA);
+  delay(500);
   EEPROM.put(8,dstAUS);
+  delay(500);
   EEPROM.put(9,output_pin_state);
-  EEPROM.commit();
+  delay(500);
+  EEPROM.end();
   update_webpage();
   server.send(200, "text/html", webpage);
 }
